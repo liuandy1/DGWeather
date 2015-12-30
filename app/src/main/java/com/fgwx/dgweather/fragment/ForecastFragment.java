058fd69c736f1d2990d8ca4d84a1c7ba39849982
燕青 <allenHu@cn.coderedfour.com>
2015-12-30 18:55:46 +0800

    在地图界面加了东西

--- app/src/main/java/com/fgwx/dgweather/fragment/ForecastFragment.java
+++ app/src/main/java/com/fgwx/dgweather/fragment/ForecastFragment.java
@@ -6,9 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.fgwx.dgweather.R;
 import com.fgwx.dgweather.activity.MainActivity;
+import com.fgwx.dgweather.utils.LogUtil;
 
 /**
  * Created by senghor on 2015/12/24.
@@ -16,6 +18,8 @@ import com.fgwx.dgweather.activity.MainActivity;
 //预报
 public class ForecastFragment extends Fragment implements View.OnClickListener{
     private MainActivity mMainActivity;
+    public static boolean isFull = false;
+    private RelativeLayout rvHomeInfo;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_forecast,null,false);
@@ -24,6 +28,9 @@ public class ForecastFragment extends Fragment implements View.OnClickListener{
     }
     private void initUi(View view){
         view.findViewById(R.id.bnt_home_more).setOnClickListener(this);
+        view.findViewById(R.id.iv_home_full).setOnClickListener(this);
+        rvHomeInfo = (RelativeLayout) view.findViewById(R.id.rv_home_info);
+
     }
 
     @Override
@@ -38,6 +45,24 @@ public class ForecastFragment extends Fragment implements View.OnClickListener{
             case R.id.bnt_home_more:
                mMainActivity.toggleMoreFragment(true);
                 break;
+
+            case R.id.iv_home_full:
+                fullWindow();
+                break;
+        }
+    }
+
+    /**
+     * 开启或者关闭全屏
+     */
+    private void fullWindow() {
+        LogUtil.e("全屏操作");
+        if(!isFull){
+            rvHomeInfo.setVisibility(View.GONE);
+            isFull = true;
+        }else {
+            rvHomeInfo.setVisibility(View.VISIBLE);
+            isFull = false;
         }
     }
 }
