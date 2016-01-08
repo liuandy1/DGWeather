ab2cffff787bbc860022864667e046705cdb334a
shanguangliu <shanguangliu@sohu-inc.com>
2016-01-08 19:09:25 +0800

    首页

---
+++ app/src/main/java/com/fgwx/dgweather/adapter/ForecastSortAdapter.java
@@ -0,0 +1,44 @@
+package com.fgwx.dgweather.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fgwx.dgweather.view.PagerAdapter;
+
+import java.util.List;
+
+/**
+ * Class description goes here.
+ *
+ * @author liu shanguang (account@sohu-inc.com)
+ */
+public class ForecastSortAdapter extends PagerAdapter {
+
+    private List<View> mListViews;
+
+    public ForecastSortAdapter(List<View> mListViews) {
+        this.mListViews = mListViews;//构造方法，参数是我们的页卡，这样比较方便。
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object)   {
+        container.removeView(mListViews.get(position));//删除页卡
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {  //这个方法用来实例化页卡
+        container.addView(mListViews.get(position), 0);//添加页卡
+        return mListViews.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return  mListViews.size();//返回页卡的数量
+    }
+
+    @Override
+    public boolean isViewFromObject(View arg0, Object arg1) {
+        return arg0==arg1;//官方提示这样写
+    }
+
+}
---
+++ app/src/main/java/com/fgwx/dgweather/view/PerDayWeatherView.java
@@ -0,0 +1,44 @@
+package com.fgwx.dgweather.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.fgwx.dgweather.R;
+
+/**
+ * Class description goes here.
+ *
+ * @author liu shanguang (account@sohu-inc.com)
+ */
+public class PerDayWeatherView extends RelativeLayout{
+
+    private TextView mTvWeatherDescrip;
+    private ImageView mIvWeatherIcon;
+    private TextView mTvWindDescrip;
+    private TextView mTvTemDescrip;
+    public PerDayWeatherView(Context context) {
+        this(context, null);
+    }
+    public PerDayWeatherView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+    public PerDayWeatherView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        init();
+    }
+    private void init() {
+        LayoutInflater.from(getContext()).inflate(R.layout.layout_per_day_weather_item, this);
+        mIvWeatherIcon= (ImageView) findViewById(R.id.iv_icon_per_day_weather);
+        mTvTemDescrip= (TextView) findViewById(R.id.iv_per_day_weather_temp);
+        mTvWindDescrip=(TextView)findViewById(R.id.tv_per_day_weather_wind_description);
+    }
+}
