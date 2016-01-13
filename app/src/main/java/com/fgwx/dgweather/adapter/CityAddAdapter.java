1e95cd4f09bca3f4ce93a6cd648e80f28ab421f0
燕青 <allenHu@cn.coderedfour.com>
2016-01-13 19:52:45 +0800

    添加了城市管理和添加城市的界面。合并了姚的分享的代码 但是还有bug

---
+++ app/src/main/java/com/fgwx/dgweather/adapter/CityAddAdapter.java
@@ -0,0 +1,68 @@
+package com.fgwx.dgweather.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.fgwx.dgweather.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
+
+/**
+ * Created by AllenHu on 2016/1/13.
+ */
+public class CityAddAdapter extends BaseAdapter {
+    private List list;
+    private Context context;
+
+    public CityAddAdapter(List list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    public CityAddAdapter(String[] citys, Context context) {
+        this.list = new ArrayList();
+        for (String s:citys){
+            this.list.add(s);
+        }
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.layout_add_city_dg, null);
+
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.text);
+        String city = (String) list.get(position);
+        textView.setText(city);
+        textView.setLayoutParams(new GridView.LayoutParams(75, 75));//设置ImageView对象布局
+        textView.setPadding(8, 8, 8, 8);//设置间距
+//        imageView.setImageResource(imgs[position]);//为ImageView设置图片资源
+        return convertView;
+    }
+}
--- app/src/main/java/com/fgwx/dgweather/view/ForecastFirstView.java
+++ app/src/main/java/com/fgwx/dgweather/view/ForecastFirstView.java
@@ -34,6 +34,7 @@ import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption;
 import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
 import com.fgwx.dgweather.R;
 import com.fgwx.dgweather.activity.AddCityActivity;
+import com.fgwx.dgweather.activity.CityManagerActivity;
 import com.fgwx.dgweather.activity.MainActivity;
 import com.fgwx.dgweather.adapter.MyPagerAdapter;
 import com.fgwx.dgweather.bean.ForecastMonitorSiteBean;
@@ -155,6 +156,7 @@ public class ForecastFirstView extends RelativeLayout implements View.OnClickLis
 
     private void initUi(View view) {
         rvLocal = (RelativeLayout) view.findViewById(R.id.rv_top_local);
+        rvLocal.setOnClickListener(this);
         view.findViewById(R.id.iv_home_full).setOnClickListener(this);
         view.findViewById(R.id.iv_home_location).setOnClickListener(this);
         view.findViewById(R.id.ly_home_down).setOnClickListener(this);
@@ -331,7 +333,7 @@ public class ForecastFirstView extends RelativeLayout implements View.OnClickLis
         tvHomeCity.setText(addressDetail.district);
         isSetLoc = true;
         LogUtil.e("位置是:" + reverseGeoCodeResult.getAddress());
-        MPreferencesUtil.getInstance().setValue(Constant.NOWLOCAL,reverseGeoCodeResult.getAddress());
+        MPreferencesUtil.getInstance().setValue(Constant.NOWLOCAL, reverseGeoCodeResult.getAddress());
         String city = addressDetail.city;
         String district = addressDetail.district;
         String street = addressDetail.street;
@@ -362,6 +364,47 @@ public class ForecastFirstView extends RelativeLayout implements View.OnClickLis
 
 
     /**
+     * 带参数的分享
+     * area   地区
+     * weaDesc   天气描述
+     * tempRange   温度范围
+     * windDirec   风向
+     * windSpeed   风速
+     */
+    private void showShare(String area, String weaDesc, String tempRange, String windDirec, String windSpeed) {
+        ShareSDK.initSDK(mMainActivity);
+        cn.sharesdk.onekeyshare.OnekeyShare oks = new cn.sharesdk.onekeyshare.OnekeyShare();
+        // 关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // 分享时Notification的图标和文字 2.5.9以后的版本不调用此方法
+        // oks.setNotification(R.drawable.ic_launcher,
+        // getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(mMainActivity.getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://mob.com");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("亲！好的天气带来好的心情！东莞天气提醒您，" + area + "今天的天气是" + weaDesc + "，" + tempRange + "，" + windDirec + "，"
+                + windSpeed + "。\n");
+        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+//        oks.setImagePath("/sdcard/test.jpg");// 确保SDcard下面存在此张图片
+
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("这是评论");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(mMainActivity.getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+        // 启动分享GUI
+        oks.show(mMainActivity);
+    }
+
+    /**
      * 分享
      */
     private void showShare() {
@@ -382,8 +425,8 @@ public class ForecastFirstView extends RelativeLayout implements View.OnClickLis
         oks.setImagePath("/sdcard/test.jpg");// 确保SDcard下面存在此张图片
         // url仅在微信（包括好友和朋友圈）中使用
         oks.setUrl("http://sharesdk.cn");
-                // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-                oks.setComment("我是测试评论文本");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
         // site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite(mMainActivity.getString(R.string.app_name));
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
@@ -456,11 +499,12 @@ public class ForecastFirstView extends RelativeLayout implements View.OnClickLis
                     Toast.makeText(getContext(), "当前网络不可用", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.rv_top_local:
-                mMainActivity.startActivity(new Intent(mMainActivity, AddCityActivity.class));
+                Intent intent = new Intent(mMainActivity, CityManagerActivity.class);
+                mMainActivity.startActivity(intent);
                 break;
 
             case R.id.ib_home_share:
-                showShare();
+                showShare("东莞", "多云转晴", "21~30度", "东南风", "3~4级");
                 break;
         }
     }
