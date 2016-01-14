56b120a93ab286601bf4d42b917f519130510516
yaozqun <846532201@qq.com>
2016-01-14 23:19:22 +0800

    添加城市

--- app/src/main/java/com/fgwx/dgweather/adapter/CityAddAdapter.java
+++ app/src/main/java/com/fgwx/dgweather/adapter/CityAddAdapter.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fgwx.dgweather.R;
+import com.fgwx.dgweather.utils.LogUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,14 +22,14 @@ public class CityAddAdapter extends BaseAdapter {
     private List list;
     private Context context;
 
-    public CityAddAdapter(List list, Context context) {
+    public CityAddAdapter(Context context, List list) {
         this.list = list;
         this.context = context;
     }
 
-    public CityAddAdapter(String[] citys, Context context) {
+    public CityAddAdapter(Context context, String[] citys) {
         this.list = new ArrayList();
-        for (String s:citys){
+        for (String s : citys) {
             this.list.add(s);
         }
         this.context = context;
@@ -46,22 +47,18 @@ public class CityAddAdapter extends BaseAdapter {
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.layout_add_city_dg, null);
-
+            convertView = LayoutInflater.from(context).inflate(R.layout.activity_add_city_item, null);
         }
-        TextView textView = (TextView) convertView.findViewById(R.id.text);
+        TextView textView = (TextView) convertView.findViewById(R.id.tv_city);
         String city = (String) list.get(position);
         textView.setText(city);
-        textView.setLayoutParams(new GridView.LayoutParams(75, 75));//设置ImageView对象布局
-        textView.setPadding(8, 8, 8, 8);//设置间距
-//        imageView.setImageResource(imgs[position]);//为ImageView设置图片资源
         return convertView;
     }
 }
