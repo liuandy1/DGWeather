34d3d6b0efddb719194e900722d7a8158456fd53
yaozqun <846532201@qq.com>
2016-01-17 23:01:03 +0800

    城市管理

--- app/src/main/java/com/fgwx/dgweather/activity/CityManagerActivity.java
+++ app/src/main/java/com/fgwx/dgweather/activity/CityManagerActivity.java
@@ -4,30 +4,50 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import com.fgwx.dgweather.R;
 import com.fgwx.dgweather.base.BaseActivity;
+import com.fgwx.dgweather.view.DeleteListView;
 
-public class CityManagerActivity extends BaseActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CityManagerActivity extends BaseActivity {
+    private DeleteListView dlv_cityManager;
+    private ArrayAdapter<String> mAdapter;
+    private List<String> mDatas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_manager);
-        //findViewById(R.id.btn_add_city).setOnClickListener(this);
+        dlv_cityManager = (DeleteListView) findViewById(R.id.dlv_cityManager);
+
+        // 不要直接Arrays.asList
+        mDatas = new ArrayList<String>(Arrays.asList("HelloWorld", "Welcome", "Java", "Android", "Servlet", "Struts",
+                "Hibernate", "Spring", "HTML5", "Javascript", "Lucene"));
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDatas);
+        dlv_cityManager.setAdapter(mAdapter);
+
+        dlv_cityManager.setDelButtonClickListener(new DeleteListView.DelButtonClickListener() {
+            @Override
+            public void clickHappend(final int position) {
+                mAdapter.remove(mAdapter.getItem(position));
+            }
+        });
+
+        dlv_cityManager.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-
-        switch (v.getId()) {
-           /* case R.id.btn_add_city:
-//                startActivity(new Intent(this, AddCityActivity.class));
-                break;*/
-        }
-    }
-    public static void starCityManagerActivity(Context context){
-        Intent intent=new Intent(context,CityManagerActivity.class);
+    public static void starCityManagerActivity(Context context) {
+        Intent intent = new Intent(context, CityManagerActivity.class);
         context.startActivity(intent);
     }
 }
